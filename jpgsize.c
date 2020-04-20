a3518da071f03194e9d3e9c41c113b8dc9be21e9
PolkaDott <andrey.gurt@yandex.ru>
2020-04-21 01:52:37 +0300

    updated to c

---
+++ jpgsize.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int GetImageSize(const char *fn, int *x,int *y)
+{
+    FILE *f=fopen(fn,"rb");
+    if (f==0) return 1;
+    fseek(f,0,SEEK_END);
+    long len=ftell(f);
+    fseek(f,0,SEEK_SET);
+    if (len<24) {
+        fclose(f);
+        return 1;
+        }
+  // Strategy:
+  // reading GIF dimensions requires the first 10 bytes of the file
+  // reading PNG dimensions requires the first 24 bytes of the file
+  // reading JPEG dimensions requires scanning through jpeg chunks
+  // In all formats, the file is at least 24 bytes big, so we'll read that always
+  unsigned char buf[24]; fread(buf,1,24,f);
+
+  // For JPEGs, we need to read the first 12 bytes of each chunk.
+  // We'll read those 12 bytes at buf+2...buf+14, i.e. overwriting the existing buf.
+  if (buf[0]==0xFF && buf[1]==0xD8 && buf[2]==0xFF && buf[3]==0xE0 && buf[6]=='J' && buf[7]=='F' && buf[8]=='I' && buf[9]=='F')
+  { long pos=2;
+    while (buf[2]==0xFF)
+    { if (buf[3]==0xC0 || buf[3]==0xC1 || buf[3]==0xC2 || buf[3]==0xC3 || buf[3]==0xC9 || buf[3]==0xCA || buf[3]==0xCB) break;
+      pos += 2+(buf[4]<<8)+buf[5];
+      if (pos+12>len) break;
+      fseek(f,pos,SEEK_SET); fread(buf+2,1,12,f);
+    }
+  }
+
+  fclose(f);
+
+  // JPEG: (first two bytes of buf are first two bytes of the jpeg file; rest of buf is the DCT frame
+  if (buf[0]==0xFF && buf[1]==0xD8 && buf[2]==0xFF)
+  { 
+	*y = (buf[7]<<8) + buf[8];
+    *x = (buf[9]<<8) + buf[10];
+    //cout << *x << endl;
+    return 0;
+  }
+  return 1;
+}
+
+int main(int argc, char**argv)
+{
+	if (argc < 2) return 1;
+	const char *theFile = argv[1];
+	int the_x =0;
+	int the_y =0;
+	GetImageSize(theFile, &the_x, &the_y);
+    printf("%dx%d", the_x, the_y);
+    return 0;
+}
+/*
+sourse from site
+https://www.cplusplus.com/forum/beginner/45217/
+it is cutted and works only with jpeg
+*/
